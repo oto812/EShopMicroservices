137b3848560261dbc55accd58c3926e49daa0824
otari abashidze <abashidzeoto@gmail.com>
2026-02-04 14:25:06 +0400

    feat : implemented get products endpoint and handler

---
+++ src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsEndpoint.cs
@@ -0,0 +1,25 @@
+﻿
+namespace Catalog.API.Products.GetProducts;
+
+
+
+public record GetproductsResponse(IEnumerable<Product> Products);
+public class GetProductsEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/products", async (ISender sender) =>
+        {
+            var result = await sender.Send(new GetProductsQuery());
+
+            var response = result.Adapt<GetproductsResponse>();
+
+            return Results.Ok(response);
+        })
+        .WithName("GetProducts")
+        .Produces<GetproductsResponse>(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .WithSummary("Get products")
+        .WithDescription("Get products");
+    }
+}
