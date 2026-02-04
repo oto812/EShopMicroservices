33441de7ee57d6216ea27f77c1dfdc3a0958668a
otari abashidze <abashidzeoto@gmail.com>
2026-02-04 15:34:33 +0400

    feat: added implementation of GetProductByCategory endpoint and handler

---
+++ src/Services/Catalog/Catalog.API/Products/GetProductByCategory/GetProductByCategoryHandler.cs
@@ -0,0 +1,22 @@
+﻿namespace Catalog.API.Products.GetProductByCategory;
+
+public record GerProductByCategoryQuery(string Category) : IQuery<GetProductByCategoryResult>;
+public record GetProductByCategoryResult(IEnumerable<Product> Products);
+internal class GetProductByCategoryHandler
+    (IDocumentSession session, ILogger<GetProductByCategoryHandler> logger)
+    : IQueryHandler<GerProductByCategoryQuery, GetProductByCategoryResult>
+{
+    public async Task<GetProductByCategoryResult> Handle(GerProductByCategoryQuery query, CancellationToken cancellationToken)
+    {
+        logger.LogInformation($"GetProductByCategoryHandler. handle called with {query}");
+
+        var products = await session.Query<Product>()
+            .Where(p => p.Category.Contains(query.Category)).ToListAsync();
+
+        if(products is null)
+        {
+            throw new ProductNotFoundException();
+        }
+        return new GetProductByCategoryResult(products);
+    }
+}
