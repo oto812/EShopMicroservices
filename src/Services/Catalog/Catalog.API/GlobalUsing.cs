e29dd398e0574d1a56c5c00fdd592d320816037d
otari abashidze <abashidzeoto@gmail.com>
2026-02-06 21:29:52 +0400

    feat: seeded database with data and added pagination to GetProducts query

--- src/Services/Catalog/Catalog.API/GlobalUsing.cs
+++ src/Services/Catalog/Catalog.API/GlobalUsing.cs
@@ -7,5 +7,5 @@ global using Marten;
 global using FluentValidation;
 global using BuildingBlocks.Behaviors;
 global using BuildingBlocks.Exceptions.Handler;
-
+global using Catalog.API.Data;
 
--- src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsEndpoint.cs
+++ src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsEndpoint.cs
@@ -1,23 +1,24 @@
 ﻿
 namespace Catalog.API.Products.GetProducts;
 
+public record GetProductsRequest(int? PageNumber = 1, int? PageSize = 10);
 
-
-public record GetproductsResponse(IEnumerable<Product> Products);
+public record GetProductsResponse(IEnumerable<Product> Products);
 public class GetProductsEndpoint : ICarterModule
 {
     public void AddRoutes(IEndpointRouteBuilder app)
     {
-        app.MapGet("/products", async (ISender sender) =>
+        app.MapGet("/products", async ([AsParameters] GetProductsRequest request, ISender sender) =>
         {
-            var result = await sender.Send(new GetProductsQuery());
+            var query = request.Adapt<GetProductsQuery>();
+            var result = await sender.Send(query);
 
-            var response = result.Adapt<GetproductsResponse>();
+            var response = result.Adapt<GetProductsResponse>();
 
             return Results.Ok(response);
         })
         .WithName("GetProducts")
-        .Produces<GetproductsResponse>(StatusCodes.Status200OK)
+        .Produces<GetProductsResponse>(StatusCodes.Status200OK)
         .ProducesProblem(StatusCodes.Status400BadRequest)
         .WithSummary("Get products")
         .WithDescription("Get products");
