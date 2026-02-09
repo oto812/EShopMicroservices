ae1dfe32da203af016fa8e59ae77e921eecef05f
otari abashidze <abashidzeoto@gmail.com>
2026-02-09 16:39:22 +0400

    feat: developed DeleteBasket handler and endpoint

--- src/Services/Basket/Basket.API/Basket/DeleteBasket/DeleteBasketHandler.cs
+++ src/Services/Basket/Basket.API/Basket/DeleteBasket/DeleteBasketHandler.cs
@@ -1,5 +1,29 @@
-﻿namespace Basket.API.Basket.DeleteBasket;
+﻿
+namespace Basket.API.Basket.DeleteBasket;
 
-public class DeleteBasketHandler
+
+public record DeleteBasketCommand(string UserName) : ICommand<DeleteBasketResult>;
+
+public record DeleteBasketResult(bool IsDeleted);
+
+public class DeleteBasketCommandValidator : AbstractValidator<DeleteBasketCommand>
 {
+    public DeleteBasketCommandValidator()
+    {
+        RuleFor(x => x.UserName).NotEmpty().WithMessage("User name is required.");
+}
+}
+public class DeleteBasketCommandHandler : ICommandHandler<DeleteBasketCommand, DeleteBasketResult>
+
+{
+    public async Task<DeleteBasketResult> Handle(DeleteBasketCommand request, CancellationToken cancellationToken)
+    {
+
+        //TODO : delete basket from database and cache
+        // session.Delete<Product>(command.Id)
+
+        return new DeleteBasketResult(true);
+    }
+
+
 }
--- src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketEndpoint.cs
+++ src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketEndpoint.cs
@@ -1,5 +1,4 @@
-﻿
-namespace Basket.API.Basket.StoreBasket;
+﻿namespace Basket.API.Basket.StoreBasket;
 
 public record StoreBasketRequest(ShoppingCart Cart) : ICommand<StoreBasketResponse>;
 
