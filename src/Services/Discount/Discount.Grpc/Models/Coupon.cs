db15665fcb1bf69ed014297d020eabb7b2236ab8
otari abashidze <abashidzeoto@gmail.com>
2026-02-10 18:57:29 +0400

    feat: developed discount.proto file

--- src/Services/Discount/Discount.Grpc/Models/Coupon.cs
+++ src/Services/Discount/Discount.Grpc/Models/Coupon.cs
@@ -3,7 +3,7 @@
 public class Coupon
 {
     public int Id { get; set; }
-    public string ProductName { get; set; }
-    public string Description { get; set; }
+    public string ProductName { get; set; } = default!;
+    public string Description { get; set; } = default!;
     public decimal Amount { get; set; }
 }
