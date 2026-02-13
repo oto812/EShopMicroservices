d3841fefb05911e3a8f03cd50f56cd37bdaea066
otari abashidze <abashidzeoto@gmail.com>
2026-02-13 20:04:34 +0400

    feat: created ordering microservice with clean architecture

---
+++ src/Services/Ordering/Ordering.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,17 @@
+﻿namespace Ordering.Infrastructure;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        
+        var connectionString = configuration.GetConnectionString("Database");
+
+        //services.AddDbContext<OrderingDbContext>(options =>
+        //    options.UseSqlServer(connectionString));
+
+        return services;
+    }
+}
