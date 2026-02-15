93ad014101d22b9558805db47f1f05504f14cb1c
otari abashidze <abashidzeoto@gmail.com>
2026-02-15 17:16:42 +0400

    feat:developed strongly typed ID-s for domain Entities

--- src/Services/Ordering/Ordering.Domain/Models/Customer.cs
+++ src/Services/Ordering/Ordering.Domain/Models/Customer.cs
@@ -2,9 +2,9 @@
 
 namespace Ordering.Domain.Models;
 
-public class Customer : Entity<Guid>
+public class Customer : Entity<CustomerId>
 {
     public string Name { get; private set; } = default!;
 
-    public string Emaul { get; private set; } = default!;
+    public string Email { get; private set; } = default!;
 }
