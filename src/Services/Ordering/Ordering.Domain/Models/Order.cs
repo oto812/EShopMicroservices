9caf87f46b038219ace442252a9f4214dab66ae2
otari abashidze <abashidzeoto@gmail.com>
2026-02-16 14:31:51 +0400

    feat: implemented crud methods of order in order Entity

--- src/Services/Ordering/Ordering.Domain/Models/Order.cs
+++ src/Services/Ordering/Ordering.Domain/Models/Order.cs
@@ -7,6 +7,7 @@ public class Order : Aggregate<OrderId>
     public IReadOnlyList<OrderItem> OrderItems => _orderItems.AsReadOnly();
     public CustomerId CustomerId { get; private set; } = default!;
     public OrderName OrderName { get; private set; } = default!;
+    public Address ShippingAddress { get; private set; } = default!
     public Address BillingAddress { get; private set; } = default!;
     public Payment Payment { get; private set; } = default!;
     public OrderStatus Status { get; private set; } = default!;
@@ -16,6 +17,58 @@ public class Order : Aggregate<OrderId>
         get => OrderItems.Sum(x => x.Price * x.Quantity);
         private set { }
     }
+
+    public static Order Create(OrderId id, CustomerId cutomerId, OrderName orderName, Address shippingAddress, Address BillingAddress, Payment payment)
+    {
+        var order = new Order
+        {
+            Id = id,
+            CustomerId = cutomerId,
+            OrderName = orderName,
+            ShippingAddress = shippingAddress,
+            BillingAddress = BillingAddress,
+            Payment = payment,
+            Status = OrderStatus.Pending
+        };
+
+        order.AddDomainEvent(new OrderCtearedEvent(order));
+
+        return order;
+
+    }
+    public void Update(CustomerId cutomerId, OrderName orderName, Address shippingAddress, Address BillingAddress, Payment payment, OrderStatus status)
+    {
+
+        CustomerId = cutomerId;
+        OrderName = orderName;
+        ShippingAddress = shippingAddress;
+        BillingAddress = BillingAddress;
+        Payment = payment;
+        Status = status;
+    }
+
+
+    public void Add(ProductId productId, int quantity, decimal price)
+    {
+        ArgumentOutOfRangeException.ThrowIfNegativeOrZero(quantity);
+        ArgumentOutOfRangeException.ThrowIfNegativeOrZero(price);
+
+        var orderItem = new OrderItem(Id, productId, quantity, price);
+        _orderItems.Add(orderItem);
+    }
+
+    public void Remove(ProductId productId)
+    {
+        var orderItem = _orderItems.FirstOrDefault(x => x.ProductId == productId);
+
+        if (orderItem != null)
+        {
+            _orderItems.Remove(orderItem);
+        }
+
+    }
+
+
 
 
 
