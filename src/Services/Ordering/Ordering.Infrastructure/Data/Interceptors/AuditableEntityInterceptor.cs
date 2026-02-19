a29d9645a286f919b48547e284d06ddff79c04b3
otari abashidze <abashidzeoto@gmail.com>
2026-02-19 14:19:52 +0400

    feat: added interceptors for auditing ordering entities

---
+++ src/Services/Ordering/Ordering.Infrastructure/Data/Interceptors/AuditableEntityInterceptor.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Diagnostics;
+
+namespace Ordering.Infrastructure.Data.Interceptors;
+
+public class AuditableEntityInterceptor : SaveChangesInterceptor
+{
+    public override InterceptionResult<int> SavingChanges(DbContextEventData eventData, InterceptionResult<int> result)
+    {
+        UpdateEntites(eventData.Context);
+        return base.SavingChanges(eventData, result);
+    }
+
+    public override ValueTask<InterceptionResult<int>> SavingChangesAsync(DbContextEventData eventData, InterceptionResult<int> result, CancellationToken cancellationToken = default)
+    {
+        return base.SavingChangesAsync(eventData, result, cancellationToken);
+    }
+
+    public void UpdateEntites(DbContext? context)
+    {
+        if (context == null) return;
+        foreach(var entry in context.ChangeTracker.Entries<IEntity>())
+        {
+            if(entry.State == EntityState.Added)
+            {
+                entry.Entity.CreatedAt = DateTime.UtcNow;
+                entry.Entity.CreatedBy = "System";
+            }
+            if(entry.State == EntityState.Added || entry.State == EntityState.Modified || entry.HasChangedOwnedEntities())
+            {
+                entry.Entity.LastModified = DateTime.UtcNow;
+                entry.Entity.LastModifiedBy = "System";
+            }
+        }
+        
+    }
+}
+
+public static class Extensions
+{
+    public static bool HasChangedOwnedEntities(this Microsoft.EntityFrameworkCore.ChangeTracking.EntityEntry entry)
+    {
+        return entry.References.Any(r => r.TargetEntry != null && r.TargetEntry.Metadata.IsOwned() && (r.TargetEntry.State == EntityState.Added || r.TargetEntry.State == EntityState.Modified));
+    }
+}
