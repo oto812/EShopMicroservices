6eb9203458c6414ad308329b5a355daf4007a388
otari abashidze <abashidzeoto@gmail.com>
2026-02-21 15:38:07 +0400

    feat: developed UpdatedOrder and DeleteOrder endpoints

--- src/Services/Ordering/Ordering.API/Endpoints/DeleteOrder.cs
+++ src/Services/Ordering/Ordering.API/Endpoints/DeleteOrder.cs
@@ -1,5 +1,28 @@
-﻿namespace Ordering.API.Endpoints;
+﻿
+using Ordering.Application.Orders.Commands.DeleteOrder;
 
-public class DeleteOrder
+namespace Ordering.API.Endpoints;
+
+
+public record DeleteOrderResponse(bool IsSuccess);
+public class DeleteOrder : ICarterModule
 {
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapDelete("orders/{id}", async (Guid id, ISender sender) =>
+        {
+            var result = sender.Send(new DeleteOrderCommand(id));
+
+            var response = result.Adapt<DeleteOrderResponse>();
+            
+            return Results.Ok(response);
+        })
+        .WithName("Delete Order")
+        .Produces<DeleteOrderResponse>(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .ProducesProblem(StatusCodes.Status404NotFound)
+        .WithSummary("Delete Order")
+        .WithDescription("Delete Order");
+
+    }
 }
