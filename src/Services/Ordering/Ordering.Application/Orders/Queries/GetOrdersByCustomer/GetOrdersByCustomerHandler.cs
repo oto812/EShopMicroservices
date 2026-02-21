d57b0ea6baf15d648ad0152a6fbd9a2f55dd0c4b
otari abashidze <abashidzeoto@gmail.com>
2026-02-21 13:10:15 +0400

    feat: developed GetOrderByCustomer query and handler in Ordering.Application

---
+++ src/Services/Ordering/Ordering.Application/Orders/Queries/GetOrdersByCustomer/GetOrdersByCustomerHandler.cs
@@ -0,0 +1,17 @@
+﻿namespace Ordering.Application.Orders.Queries.GetOrdersByCustomer;
+
+public class GetOrdersByCustomerHandler
+    (IApplicationDbContext dbContext): IQueryHandler<GetOrdersByCustomerQuery, GetOrdersByCustomerResult>
+{
+    public async Task<GetOrdersByCustomerResult> Handle(GetOrdersByCustomerQuery request, CancellationToken cancellationToken)
+    {
+        var orders = await dbContext.Orders
+            .Include(o => o.OrderItems)
+            .AsNoTracking()
+            .Where(o => o.CustomerId == CustomerId.Of(request.CustomerId))
+            .OrderBy(o => o.OrderName.Value)
+            .ToListAsync();
+
+        return new GetOrdersByCustomerResult(orders.ToOrderDtoList());
+    }
+}
