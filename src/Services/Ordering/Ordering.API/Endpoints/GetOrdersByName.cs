545c0a98614c4b39537fc0878db03ebb9fd62a56
otari abashidze <abashidzeoto@gmail.com>
2026-02-21 15:53:57 +0400

    feat: developed GetOrdersByName endpoint

--- src/Services/Ordering/Ordering.API/Endpoints/GetOrdersByName.cs
+++ src/Services/Ordering/Ordering.API/Endpoints/GetOrdersByName.cs
@@ -1,5 +1,27 @@
-﻿namespace Ordering.API.Endpoints;
+﻿
+using Ordering.Application.Orders.Queries.GetOrdersByName;
 
-public class GetOrdersByName
+namespace Ordering.API.Endpoints;
+
+public record GetOrdersByNameResponse(IEnumerable<OrderDto> Orders);
+
+
+public class GetOrdersByName : ICarterModule
 {
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("orders/{orderName}", async (string orderName, ISender sender) =>
+        {
+            var result = await sender.Send(new GetOrdersByNameQuery(orderName));
+
+            var response = result.Adapt<GetOrdersByName>();
+
+            return Results.Ok(response);
+        })
+         .WithName("Get Orders By Name")
+        .Produces<UpdateOrderResponse>(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .WithSummary("Get Orders By Name")
+        .WithDescription("Get Orders By Name");
+    }
 }
