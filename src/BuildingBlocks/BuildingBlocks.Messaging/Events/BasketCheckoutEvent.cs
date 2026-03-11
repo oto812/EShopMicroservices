6e25a025db0ca275460b49887010c957f592821d
otari abashidze <abashidzeoto@gmail.com>
2026-03-11 15:05:08 +0400

    feat: developed BasketCheckoutEvent consumer Integration event handler class

--- src/BuildingBlocks/BuildingBlocks.Messaging/Events/BasketCheckoutEvent.cs
+++ src/BuildingBlocks/BuildingBlocks.Messaging/Events/BasketCheckoutEvent.cs
@@ -20,7 +20,7 @@ public record BasketCheckoutEvent : IntegrationEvent
     public string CardNumber { get; set; } = default!;
     public string Expiration { get; set; } = default!;  
     public string CVV { get; set; } = default!;
-    public string PaymentMethod { get; set; } = default!;
+    public int PaymentMethod { get; set; } = default!;
 
 
 
